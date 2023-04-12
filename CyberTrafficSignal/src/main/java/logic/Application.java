30e019ea96a93767b130903e0d464980022ea3a1
ksharm53 <ksharm53@asu.edu>
2023-04-12 10:55:50 -0700

    Initial Commit

---
+++ CyberTrafficSignal/src/main/java/logic/Application.java
@@ -0,0 +1,18 @@
+package logic;
+
+public class Application {
+	
+	EmergencyVehiclePolling evp;
+	VehicleCounter vc;
+	DetermineDirection dd;
+	CriticalFlow cf;
+	Webster webster;
+	DataProcessingController dpc;
+	LightSignalController lsc;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
