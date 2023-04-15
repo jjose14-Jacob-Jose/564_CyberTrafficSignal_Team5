f2096e0821d708f829c2f750797f05279af74f50
Jacob Jose <jjose14@asu.edu>
2023-04-14 20:53:44 -0700

    20230414_2053 Added jUnit dependency to 'pom.xml'. Added a jUnit test class.

---
+++ CyberTrafficSignal/src/test/logic/ApplicationTest.java
@@ -0,0 +1,14 @@
+package logic;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+}
