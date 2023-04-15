7e7407e1f9d1e74f89d75040bd5374d9b50dd1f2
Jacob Jose <jjose14@asu.edu>
2023-04-14 23:05:26 -0700

    20230414_2304 Remove file reading from ParseData.java.

--- CyberTrafficSignal/src/main/java/logic/ParseData.java
+++ CyberTrafficSignal/src/main/java/logic/ParseData.java
@@ -1,21 +1,33 @@
 package logic;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.util.ResourceUtils;
+//	Start: Line commented because Jacob's laptop couldn't find  ResourceUtils.java.
+//import org.springframework.util.ResourceUtils;
+//	End: Line commented because Jacob's laptop couldn't find  ResourceUtils.java.
 
 public class ParseData {
 
 	public static List<InputData> parseFile(String filePath) throws IOException {
 
 		List<InputData> list = new ArrayList<>();
-		File file = ResourceUtils.getFile("classpath:inputData.txt");
 
-		List<String> content = Files.readAllLines(file.toPath());
+//	Start: Line commented because Jacob's laptop couldn't find  ResourceUtils.java.
+//		File file = ResourceUtils.getFile("classpath:inputData.txt");
+//
+//		List<String> content = Files.readAllLines(file.toPath());
+
+//	End: Line commented because Jacob's laptop couldn't find  ResourceUtils.java.
+
+		List<String> content = new ArrayList<>(Arrays.asList(
+				"15,0,15,0,20,0,25,0,1000,1250,false,false,false,false",
+				"50,4,50,4,20,10,20,10,1000,1250,false,false,true,false",
+				"5,0,5,0,5,0,5,0,1000,1250,false,false,false,false"
+		));
+
 
 		for (int i = 0; i < content.size(); i++) {
 
