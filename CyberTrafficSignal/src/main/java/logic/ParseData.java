d9b254c12e780b9f7b3b8585455a4f40b346defc
Jacob Jose <jjose14@asu.edu>
2023-04-16 14:09:08 -0700

    20230416_1408 Commented out file reading part. Using hardcoded inputs. Working.

--- CyberTrafficSignal/src/main/java/logic/ParseData.java
+++ CyberTrafficSignal/src/main/java/logic/ParseData.java
@@ -1,12 +1,11 @@
 package logic;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.util.ResourceUtils;
+//import org.springframework.util.ResourceUtils;
 
 public class ParseData {
 
@@ -14,10 +13,27 @@ public class ParseData {
 
 	public List<InputData> parseFile(String filePath) throws IOException {
 
-		List<InputData> list = new ArrayList<>();
-		File file = ResourceUtils.getFile("classpath:inputData.txt");
+		List<InputData> list = new ArrayList<InputData>();
+//		File file = ResourceUtils.getFile("classpath:inputData.txt");
 
-		List<String> content = Files.readAllLines(file.toPath());
+//		List<String> content = Files.readAllLines(file.toPath());
+
+
+		ArrayList<String> content = new ArrayList<>(Arrays.asList(
+				"10,0,10,0,20,0,20,0,1000,1250,false,false,false,false",
+				"30,0,30,0,40,20,40,20,1000,1250,false,false,false,false",
+				"10,30,20,30,55,0,65,0,1000,1250,false,false,false,false",
+				"20,0,30,0,20,55,20,65,1000,1250,true,false,false,false",
+				"0,20,0,30,75,0,65,0,1000,1250,false,false,false,false",
+				"0,0,0,0,0,50,0,50,1000,1250,false,false,true,false",
+				"0,0,0,0,0,30,0,20,1000,1250,false,false,false,false",
+				"0,20,0,30,0,0,0,0,1000,1250,false,false,false,false",
+				"0,0,0,0,0,55,0,55,1000,1250,false,false,false,false",
+				"0,0,0,0,0,0,0,0,1000,1250,false,false,false,false",
+				"1,0,0,0,0,0,0,0,1000,1250,false,false,false,false"
+		));
+
+
 
 		for (int i = 0; i < content.size(); i++) {
 
