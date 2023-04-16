d53b416625a9579a458cc16c40551a40c5262210
Jacob Jose <jjose14@asu.edu>
2023-04-16 15:09:45 -0700

    20230416_1509 Harded code inputs for 4 scenarious. Working.

--- CyberTrafficSignal/src/main/java/logic/ParseData.java
+++ CyberTrafficSignal/src/main/java/logic/ParseData.java
@@ -19,10 +19,10 @@ public class ParseData {
 //		List<String> content = Files.readAllLines(file.toPath());
 
 
-		ArrayList<String> content = new ArrayList<>(Arrays.asList(
+		ArrayList<String> listAll = new ArrayList<>(Arrays.asList(
 				"10,0,10,0,20,0,20,0,1000,1250,false,false,false,false",
-				"30,0,30,0,40,20,40,20,1000,1250,false,false,false,false",
-				"10,30,20,30,55,0,65,0,1000,1250,false,false,false,false",
+				"5,0,10,0,10,20,15,20,1000,1250,false,false,false,false",
+				"0,15,20,30,55,0,65,0,1000,1250,false,false,false,false",
 				"20,0,30,0,20,55,20,65,1000,1250,true,false,false,false",
 				"0,20,0,30,75,0,65,0,1000,1250,false,false,false,false",
 				"0,0,0,0,0,50,0,50,1000,1250,false,false,true,false",
@@ -32,6 +32,54 @@ public class ParseData {
 				"0,0,0,0,0,0,0,0,1000,1250,false,false,false,false",
 				"1,0,0,0,0,0,0,0,1000,1250,false,false,false,false"
 		));
+
+		ArrayList<String> listNormal = new ArrayList<>(Arrays.asList(
+
+//				START: Values added by Jacob.
+//				"5,0,10,0,15,0,20,0,1000,1250,false,false,false,false",
+//				"10,0,5,0,10,15,10,20,1000,1250,false,false,false,false",
+//				"2,15,3,15,7,0,8,0,1000,1250,false,false,false,false",
+//				"8,0,9,0,1,22,1,18,1000,1250,false,false,false,false",
+//				"5,10,3,12,4,0,2,0,1000,1250,false,false,false,false"
+//				END: Values added by Jacob.
+
+				"10,0,10,0,20,0,20,0,1000,1250,false,false,false,false",
+				"5,0,10,0,10,20,15,20,1000,1250,false,false,false,false",
+				"0,15,20,30,55,0,65,0,1000,1250,false,false,false,false",
+				"20,0,30,0,20,55,20,65,1000,1250,false,false,false,false",
+				"0,0,0,0,10,20,3,20,1000,1250,false,false,false,false"
+		));
+
+
+		ArrayList<String> listEmergencySouth = new ArrayList<>(Arrays.asList(
+
+				"10,0,10,0,20,0,20,0,1000,1250,false,false,false,false",
+				"5,0,10,0,10,20,15,20,1000,1250,false,false,false,false",
+				"0,15,20,30,55,0,65,0,1000,1250,false,false,false,false",
+				"20,0,30,0,20,55,20,65,1000,1250,false,false,false,false",
+				"20,0,30,0,20,30,20,20,1000,1250,false,true,false,false",
+				"0,20,0,30,75,0,65,0,1000,1250,false,false,false,false"
+		));
+
+		ArrayList<String> listEmergencyEast = new ArrayList<>(Arrays.asList(
+
+				"10,0,10,0,20,0,20,0,1000,1250,false,false,false,false",
+				"5,0,10,0,10,20,15,20,1000,1250,false,false,false,false",
+				"15,15,20,30,15,0,15,0,1000,1250,false,false,false,false",
+				"15,0,20,0,15,30,5,30,1000,1250,false,false,true,false",
+				"0,0,0,0,0,10,0,5,1000,1250,false,false,false,false"
+				));
+
+		ArrayList<String> listEmptyNorthSouth = new ArrayList<>(Arrays.asList(
+
+				"0,0,0,0,20,0,15,0,1000,1250,false,false,false,false",
+				"0,0,0,0,5,20,8,15,1000,1250,false,false,false,false",
+				"0,0,0,0,35,5,55,15,1000,1250,false,false,false,false",
+				"5,0,10,0,0,35,55,20,1000,1250,false,false,false,false"
+		));
+
+
+		ArrayList<String> content = listEmptyNorthSouth;
 
 
 
